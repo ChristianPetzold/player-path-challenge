4f78bdfefaaf925d4d63825a26ec798ef2d0855c
Christian Petzold <github@christianpetzold.dev>
2024-08-12 21:32:01 +0200

    prepared error handling of apis

---
+++ ppc-backend/src/main/java/dev/christianpetzold/api/dto/ErrorDto.java
@@ -0,0 +1,15 @@
+package dev.christianpetzold.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorDto {
+    String errorCode;
+    String message;
+}
---
+++ ppc-backend/src/main/java/dev/christianpetzold/api/mapper/ExceptionHandlerMapper.java
@@ -0,0 +1,40 @@
+package dev.christianpetzold.api.mapper;
+
+import com.google.gson.Gson;
+import dev.christianpetzold.api.dto.ErrorDto;
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+
+@Provider
+@Priority(9999)
+public class ExceptionHandlerMapper implements ExceptionMapper<Throwable> {
+
+
+    @Override
+    @ServerExceptionMapper
+    public Response toResponse(Throwable e) {
+        Gson gson = new Gson();
+
+        if (e instanceof WebApplicationException ex) {
+            return Response.status(ex.getResponse().getStatus())
+                    .entity(
+                            gson.toJson(
+                                    ErrorDto.builder()
+                                            .errorCode(ex.getResponse().getStatusInfo().toEnum().toString())
+                                            .message(ex.getMessage())
+                                            .build()))
+                    .build();
+        } else {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(gson.toJson(ErrorDto.builder()
+                            .errorCode(String.valueOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
+                            .message("Oops! Something went wrong.")
+                            .build()))
+                    .build();
+        }
+    }
+}
