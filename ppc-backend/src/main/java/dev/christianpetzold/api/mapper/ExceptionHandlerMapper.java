fb215be50967a396565f3e4cdffb7ff14b09a00b
Christian Petzold <github@christianpetzold.dev>
2024-08-26 23:38:15 +0200

    simplified json mapping of exceptions

--- ppc-backend/src/main/java/dev/christianpetzold/api/mapper/ExceptionHandlerMapper.java
+++ ppc-backend/src/main/java/dev/christianpetzold/api/mapper/ExceptionHandlerMapper.java
@@ -1,9 +1,10 @@
 package dev.christianpetzold.api.mapper;
 
-import com.google.gson.Gson;
 import dev.christianpetzold.api.dto.ErrorDTO;
 import jakarta.annotation.Priority;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
@@ -16,21 +17,21 @@ public class ExceptionHandlerMapper implements ExceptionMapper<Throwable> {
 
     @Override
     @ServerExceptionMapper
+    @Produces(MediaType.APPLICATION_JSON)
     public Response toResponse(Throwable e) {
-        Gson gson = new Gson();
 
         if (e instanceof WebApplicationException ex) {
             return Response.status(ex.getResponse().getStatus())
                     .entity(
-                            gson.toJson(
-                                    ErrorDTO.builder()
-                                            .errorCode(ex.getResponse().getStatusInfo().toEnum().toString())
-                                            .message(ex.getMessage())
-                                            .build()))
+                            ErrorDTO.builder()
+                                    .errorCode(ex.getResponse().getStatusInfo().toEnum().toString())
+                                    .message(ex.getMessage())
+                                    .build())
                     .build();
         } else {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(gson.toJson(ErrorDTO.builder()
+                    .entity(
+                            (ErrorDTO.builder()
                             .errorCode(String.valueOf(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
                             .message("Oops! Something went wrong.")
                             .build()))
